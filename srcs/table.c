5baf46b0a94573e2fc567df2e285c190881cc56e
Clément Hamada <clementhamada@protonmail.com>
2020-12-17 12:45:03 +0100

    table: Initialize mutexes

--- srcs/table.c
+++ srcs/table.c
@@ -1,12 +1,14 @@
 #include <table.h>
 
+// TODO: Replace printfs with writes
+
 void			show_usage(const char *name)
 {
 	dprintf(2, "Usage:	%s seats time_die time_eat time_sleep [appetite]\n",
 		name);
 }
 
-bool			table_set(t_table *table, t_philo **philos, int ac, char **av)
+bool			table_new(t_table *table, t_philo **philos, int ac, char **av)
 {
 	table->appetite = 1;
 	if ((ac == 5 || (ac == 6 && (table->appetite = atoui(av[5]))))
@@ -36,6 +38,29 @@ void			table_clear(t_table *table, t_philo **philos)
 	*philos = NULL;
 }
 
+// TODO: Investigate mutex attributes
+bool			table_set(t_table *table, t_philo *philos)
+{
+	uint64_t	i;
+	int			err;
+
+	if (!(err = pthread_mutex_init(&table->write_lock, NULL)))
+	{
+		i = 0;
+		while (i < table->seats
+		&& !(err = pthread_mutex_init(&table->forks[i], NULL)))
+		{
+			philos[i].table = table;
+			philos[i].index = i;
+			i++;
+		}
+		if (!err)
+			return (true);
+	}
+	dprintf(STDERR_FILENO, "pthread_mutex_init: (%d)%s\n", err, strerror(err));
+	return (false);
+}
+
 bool			table_start(t_table *table, t_philo *philos)
 {
 	uint64_t			i;
@@ -43,16 +68,14 @@ bool			table_start(t_table *table, t_philo *philos)
 	//pthread_mutexattr_t	mutex_attr;
 	int					err;
 
+	table_set(table, philos);
 	pthread_attr_init(&thread_attr);
     pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_JOINABLE);
-	pthread_mutex_init(&table->write_lock, NULL);
 	i = 0;
 	err = 0;
 	table->time_start = clock_millis();
 	while (!err && i < table->seats)
 	{
-		philos[i].index = i;
-		philos[i].table = table;
 		err = pthread_create(&philos[i].tid, &thread_attr, &philo_thread, &philos[i]);
 		i++;
 	}
