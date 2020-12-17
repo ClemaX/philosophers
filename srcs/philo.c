2aab4146cc0e2265fdda807348a23a1e76472d56
Clément Hamada <clementhamada@protonmail.com>
2020-12-17 12:47:38 +0100

    philo: Implement basic lifecycle

--- srcs/philo.c
+++ srcs/philo.c
@@ -9,7 +9,7 @@ bool	philo_log(t_philo *philo, const char *message)
 	{
 		putui(STDOUT_FILENO, now);
 		write(STDOUT_FILENO, " ", 1);
-		putui(STDOUT_FILENO, philo->index);
+		putui(STDOUT_FILENO, philo->index + 1);
 		write(STDOUT_FILENO, " ", 1);
 		write(STDOUT_FILENO, message, ft_strlen(message));
 		write(STDOUT_FILENO, "\n", 1);
@@ -19,32 +19,55 @@ bool	philo_log(t_philo *philo, const char *message)
 	return (false);
 }
 
-
+// TODO: Fix deadlock when philos each take one fork
 void	philo_eat(t_philo *philo)
 {
-	(void)philo;
+	const size_t    forks[2] = {
+		philo->index,
+		(philo->index + 1 == philo->table->seats) ? 0 : philo->index + 1
+	};
+	int				err;
 
+	if (!(err = pthread_mutex_lock(&philo->table->forks[forks[0]])))
+	{
+		philo_log(philo, "has taken a fork");
+		if (!(err = pthread_mutex_lock(&philo->table->forks[forks[1]])))
+		{
+			philo_log(philo, "has taken a fork");
+			philo_log(philo, "is eating");
+			usleep(philo->table->time_to_eat * 1000);
+			pthread_mutex_unlock(&philo->table->forks[forks[1]]);
+		}
+		pthread_mutex_unlock(&philo->table->forks[forks[0]]);
+	}
+	if (err)
+		dprintf(STDERR_FILENO, "pthread_mutex_lock: (%d)%s, forks: %zu, %zu\n",
+			err, strerror(err), forks[0], forks[1]);
 }
 
 void	philo_sleep(t_philo *philo)
 {
-	(void)philo;
-
+	philo_log(philo, "is sleeping");
+	usleep(philo->table->time_to_sleep * 1000);
 }
 
 void	philo_think(t_philo *philo)
 {
-	(void)philo;
-
+	philo_log(philo, "is thinking");
+	usleep(10 * 1000);
 }
 
 void	*philo_thread(void *data)
 {
 	t_philo *const	philo = data;
+	uint64_t		times_ate;
 
-	philo_log(philo, "Hello from philo!");
-	philo_eat(philo);
-	philo_sleep(philo);
-	philo_think(philo);
+	times_ate = 0;
+	while (times_ate++ < philo->table->appetite)
+	{
+		philo_eat(philo);
+		philo_sleep(philo);
+		philo_think(philo);
+	}
 	return (NULL);
 }
