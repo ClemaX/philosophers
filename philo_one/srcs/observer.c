a6a7b107a3f3e55a8eb04e84bef186ac39777130
Clément Hamada <chamada@student.le-101.fr>
2021-01-02 15:00:30 +0100

    philo_one: Norme observer

--- philo_one/srcs/observer.c
+++ philo_one/srcs/observer.c
@@ -1,32 +1,35 @@
 #include <observer.h>
 
+static bool	observe_death(t_philo *philo)
+{
+	t_time	time_die;
+	bool	running;
+
+	pthread_mutex_lock(&philo->lock);
+	time_die = philo->time_die;
+	pthread_mutex_unlock(&philo->lock);
+	sleep_until(time_die);
+	pthread_mutex_lock(&philo->lock);
+	if (!(running = time_die != philo->time_die))
+	{
+		pthread_mutex_lock(&g_table.lock_run);
+		if (g_table.running)
+		{
+			table_log(philo, "died");
+			g_table.running = false;
+		}
+		pthread_mutex_unlock(&g_table.lock_run);
+	}
+	pthread_mutex_unlock(&philo->lock);
+	return (running);
+}
+
 void	*observer_thread(void *data)
 {
 	t_philo *const	philo = data;
-	t_time			time_die;
-	bool			running;
 
-	running = true;
-	while (running)
-	{
-		pthread_mutex_lock(&philo->lock);
-		time_die = philo->time_die;
-		pthread_mutex_unlock(&philo->lock);
-		sleep_until(time_die);
-		pthread_mutex_lock(&philo->lock);
-		if (time_die == philo->time_die)
-		{
-			pthread_mutex_lock(&g_table.lock_run);
-			if (g_table.running)
-			{
-				table_log(philo, "died");
-				g_table.running = false;
-			}
-			pthread_mutex_unlock(&g_table.lock_run);
-			running = false;
-		}
-		pthread_mutex_unlock(&philo->lock);
-	}
+	while (observe_death(philo))
+		;
 	pthread_join(philo->tid, &data);
 	return (data);
 }
