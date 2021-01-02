6f8db8a3f3d7ce80ce5ce2631955e4a5b7bd998f
Clément Hamada <chamada@student.le-101.fr>
2021-01-02 15:00:39 +0100

    philo_two: Norme observer

--- philo_two/srcs/observer.c
+++ philo_two/srcs/observer.c
@@ -1,32 +1,35 @@
 #include <observer.h>
 
-void	*observer_thread(void *data)
+static bool	observe_death(t_philo *philo)
+{
+	t_time	time_die;
+	bool	running;
+
+	sem_wait(philo->lock);
+	time_die = philo->time_die;
+	sem_post(philo->lock);
+	sleep_until(time_die);
+	sem_wait(philo->lock);
+	if (!(running = time_die != philo->time_die))
+	{
+		sem_wait(g_table.lock_run);
+		if (g_table.running)
+		{
+			table_log(philo, "died");
+			g_table.running = false;
+		}
+		sem_post(g_table.lock_run);
+	}
+	sem_post(philo->lock);
+	return (running);
+}
+
+void		*observer_thread(void *data)
 {
 	t_philo *const	philo = data;
-	t_time			time_die;
-	bool			running;
 
-	running = true;
-	while (running)
-	{
-		sem_wait(philo->lock);
-		time_die = philo->time_die;
-		sem_post(philo->lock);
-		sleep_until(time_die);
-		sem_wait(philo->lock);
-		if (time_die == philo->time_die)
-		{
-			sem_wait(g_table.lock_run);
-			if (g_table.running)
-			{
-				table_log(philo, "died");
-				g_table.running = false;
-			}
-			sem_post(g_table.lock_run);
-			running = false;
-		}
-		sem_post(philo->lock);
-	}
+	while (observe_death(philo))
+		;
 	pthread_join(philo->tid, &data);
 	return (data);
 }
